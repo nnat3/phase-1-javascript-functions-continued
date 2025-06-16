efb4ab9971a617e6b4d6c43424babd90db5a9597
Neathan Kungu <natekungu857@gmail.com>
2025-06-16 20:01:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+   return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visualFlair="*") {
+   return function (message='special') {
+      return `You are ${visualFlair}${message}${visualFlair}!`;
+    }
+}
+    
+
